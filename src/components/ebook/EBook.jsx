31454d807d1cc8d1de486490fd39dd855e599004
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-29 19:46:58 +0200

    Create New Book function

--- src/components/ebook/EBook.jsx
+++ src/components/ebook/EBook.jsx
@@ -1,31 +1,45 @@
-// import React, { useState } from 'react'
-import './EBook.scss'
+import React, { useState } from "react";
+import "./EBook.scss";
 
 export const EBook = () => {
+  const [books, setBooks] = useState([]);
 
-  // const [books, setBooks]=useState([])
+  const [book, setBook] = useState("");
 
-  // const [book, setBook]=useState('')
+  const handleBookChange = (e) => {
+    setBook(e.target.value);
+  };
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
+    if (book !== "") {
+      setBooks([...books, { 
+        id: books.length + 1, 
+        text: book.trim() 
+      }]);
+    }
+    setBook("");
+  };
 
   return (
     <div className="EBook">
       <h2>EBook App</h2>
-      <form>
-
+      <form onSubmit={handleFormSubmit}>
         <input
-        name="book"
-        type="text"
-        placeholder="Create a new Book "
+          name="book"
+          type="text"
+          value={book}
+          onChange={handleBookChange}
+          placeholder="Create a new Book "
         />
       </form>
+
       <div className="book-list">
-        {/* {books.map((book)=>(
-          <div>{book}</div>
-        ))} */}
+        {books.map((book) => (
+          <div key={book.id}>{book.text}</div>
+        ))}
       </div>
-      
-      </div>
-  )
-}
+    </div>
+  );
+};
