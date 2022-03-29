505405d25952f2e28491c1840495fe72cf36a804
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-29 17:23:47 +0200

    useState- Delete & emptyList

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,34 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
 import "./Books.scss";
 
 export const Books = () => {
-  const books = [
-    { _id: "b1", title: "Guide to Happines", author: "Gael" },
-    { _id: "b2", title: "Guide to JavaScript", author: "Elisa" },
-    { _id: "b3", title: "Guide to Coaching", author: "Robert" },
-  ];
+  const [books, setBooks] = useState([
+    { id: "1", title: "Guide to Happines", author: "Gael" },
+    { id: "2", title: "Guide to JavaScript", author: "Elisa" },
+    { id: "3", title: "Guide to Coaching", author: "Robert" },
+  ]);
 
-  const jsxBooks = books.map((book) => (
-    <div key={book._id} className="book">
-      <div>{book.title}</div>
-      <div>{book.author}</div>
-      <div className="icons">
-        <button type="button"></button>
-      </div>
-    </div>
-  ));
+  console.log(books);
+  const handleDelete = (id) => {
+    const listBooks = books.filter((book) => book.id !== id);
+    setBooks(listBooks);
+    localStorage.setBooks("booksList", JSON.stringify(listBooks));
+  };
 
   return (
     <div className="Books">
       <div>
-        <h2>Book List</h2>
+        <h2>Book App</h2>
       </div>
       <main>
-        <div className="container">
-          <div className="search">Search</div>
-          <div className="book-list">{jsxBooks}</div>
-          <div className="add-book"></div>
-        </div>
+        {books.length ? (
+          <div className="container">
+            <div className="search">Search</div>
+            <div className="book-list">
+              {books.map((book) => (
+                <div key={book.id} className="book">
+                  <div>{book.title}</div>
+                  <div>{book.author}</div>
+                  <div className="icons">
+                    <FaEdit className="icon" role="button" tabIndex="0" />
+                    <FaTrashAlt
+                      onClick={() => handleDelete(book.id)}
+                      className="icon"
+                      role="button"
+                      tabIndex="0"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="add-book"></div>
+          </div>
+        ) : (
+          <p style={{ marginTop: "2rem" }}> Your list is empty.</p>
+        )}
       </main>
       <footer>&copy; Copyright 2022</footer>
     </div>
