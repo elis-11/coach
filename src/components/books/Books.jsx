da035390796680724fe40a94b578bf218135eb38
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-05 17:01:51 +0200

    Create new Document

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from "react-router-dom";
 // import { State } from "./State";
 // import { UseState } from "./UseState";
-// import { DoubleBook } from "./DoubleBook";
 import { Robapp } from "./robapp/Robapp";
 import { BookApp } from "./BookApp";
 import { NewBook } from "./NewBook";
+import { DoubleBook } from "./DoubleBook";
 import "./Books.scss";
 
 export const Books = () => {
@@ -14,8 +14,8 @@ export const Books = () => {
         <Link to="">Robapp</Link>
         <Link to="bookapp">BookApp</Link>
         <Link to="newbook">NewBook</Link>
-        {/* 
         <Link to="doublebook">DoubleBook</Link>
+        {/* 
         <Link to="state">State</Link>
         <Link to="usestate">UseState</Link> */}
       </div>
@@ -24,8 +24,8 @@ export const Books = () => {
           <Route path="" element={<Robapp />} />
           <Route path="bookapp" element={<BookApp />} />
           <Route path="newbook" element={<NewBook />} />
-          {/* 
           <Route path="doublebook" element={<DoubleBook />} />
+          {/* 
           <Route path="state" element={<State />} />
           <Route path="usestate" element={<UseState />} /> */}
         </Routes>
