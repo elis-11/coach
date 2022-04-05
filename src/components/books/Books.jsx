132d8f3bb8d04b4f7b99d29138d0dbe45c2a084a
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-05 17:06:11 +0200

    Restyling

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from "react-router-dom";
 // import { State } from "./State";
-// import { UseState } from "./UseState";
 import { Robapp } from "./robapp/Robapp";
 import { BookApp } from "./BookApp";
 import { NewBook } from "./NewBook";
 import { DoubleBook } from "./DoubleBook";
+import { UseState } from "./UseState";
 import "./Books.scss";
 
 export const Books = () => {
@@ -15,9 +15,10 @@ export const Books = () => {
         <Link to="bookapp">BookApp</Link>
         <Link to="newbook">NewBook</Link>
         <Link to="doublebook">DoubleBook</Link>
+        <Link to="usestate">UseState</Link>
         {/* 
         <Link to="state">State</Link>
-        <Link to="usestate">UseState</Link> */}
+       */}
       </div>
       <div className="roures">
         <Routes>
@@ -25,9 +26,10 @@ export const Books = () => {
           <Route path="bookapp" element={<BookApp />} />
           <Route path="newbook" element={<NewBook />} />
           <Route path="doublebook" element={<DoubleBook />} />
+        <Route path="usestate" element={<UseState />} /> 
           {/* 
           <Route path="state" element={<State />} />
-          <Route path="usestate" element={<UseState />} /> */}
+        */}
         </Routes>
       </div>
     </div>
