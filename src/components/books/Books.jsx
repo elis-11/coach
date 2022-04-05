615ca00ddade9cf55715f97584b5653cdf58579f
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-05 16:51:00 +0200

    Added NewBook to tho Books

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from "react-router-dom";
 // import { State } from "./State";
-// import { NewBook } from "./NewBook";
 // import { UseState } from "./UseState";
 // import { DoubleBook } from "./DoubleBook";
 import { Robapp } from "./robapp/Robapp";
 import { BookApp } from "./BookApp";
+import { NewBook } from "./NewBook";
 import "./Books.scss";
 
 export const Books = () => {
@@ -13,7 +13,8 @@ export const Books = () => {
       <div className="links">
         <Link to="">Robapp</Link>
         <Link to="bookapp">BookApp</Link>
-        {/* <Link to="newbook">NewBook</Link>
+        <Link to="newbook">NewBook</Link>
+        {/* 
         <Link to="doublebook">DoubleBook</Link>
         <Link to="state">State</Link>
         <Link to="usestate">UseState</Link> */}
@@ -22,7 +23,8 @@ export const Books = () => {
         <Routes>
           <Route path="" element={<Robapp />} />
           <Route path="bookapp" element={<BookApp />} />
-          {/* <Route path="newbook" element={<NewBook />} />
+          <Route path="newbook" element={<NewBook />} />
+          {/* 
           <Route path="doublebook" element={<DoubleBook />} />
           <Route path="state" element={<State />} />
           <Route path="usestate" element={<UseState />} /> */}
