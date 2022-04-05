47f701548ca9691296c9d0456f0c1b310c0ef98a
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-05 16:47:46 +0200

    Added BookApp to the Books

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -3,8 +3,8 @@ import { Link, Route, Routes } from "react-router-dom";
 // import { NewBook } from "./NewBook";
 // import { UseState } from "./UseState";
 // import { DoubleBook } from "./DoubleBook";
-// import { BookApp } from "./BookApp";
 import { Robapp } from "./robapp/Robapp";
+import { BookApp } from "./BookApp";
 import "./Books.scss";
 
 export const Books = () => {
@@ -12,18 +12,18 @@ export const Books = () => {
     <div>
       <div className="links">
         <Link to="">Robapp</Link>
+        <Link to="bookapp">BookApp</Link>
         {/* <Link to="newbook">NewBook</Link>
         <Link to="doublebook">DoubleBook</Link>
-        <Link to="bookapp">BookApp</Link>
         <Link to="state">State</Link>
         <Link to="usestate">UseState</Link> */}
       </div>
       <div className="roures">
         <Routes>
           <Route path="" element={<Robapp />} />
+          <Route path="bookapp" element={<BookApp />} />
           {/* <Route path="newbook" element={<NewBook />} />
           <Route path="doublebook" element={<DoubleBook />} />
-          <Route path="bookapp" element={<BookApp />} />
           <Route path="state" element={<State />} />
           <Route path="usestate" element={<UseState />} /> */}
         </Routes>
