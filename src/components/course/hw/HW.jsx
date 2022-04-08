d00fbffbdd707ac7a397a579149dbc38d7d70d52
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-09 00:17:33 +0200

    Create HW71

--- src/components/course/hw/HW.jsx
+++ src/components/course/hw/HW.jsx
@@ -1,6 +1,7 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { HW6 } from "./HW6";
 import { HW7 } from "./HW7";
+import { HW71 } from "./HW71";
 import { HW77 } from "./HW77";
 import { HW8 } from "./HW8";
 
@@ -10,6 +11,7 @@ export const HW = () => {
       <div className="links">
         <Link to="">HW6</Link>
         <Link to="hw7">HW7</Link>
+        <Link to="hw71">HW71</Link>
         <Link to="hw77">HW77</Link>
         <Link to="hw8">HW8</Link>
       </div>
@@ -17,6 +19,7 @@ export const HW = () => {
         <Routes>
           <Route path="" element={<HW6 />} />
           <Route path="hw7" element={<HW7 />} />
+          <Route path="hw71" element={<HW71 />} />
           <Route path="hw77" element={<HW77 />} />
           <Route path="hw8" element={<HW8 />} />
         </Routes>
