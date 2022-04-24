d5e9171bf651dd14d195417df1ecb8b717508f04
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-24 12:54:24 +0200

    Modified document.

---
+++ src/components/course/rob/new copy/AddDev.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddDev = () => {
+  return (
+    <div>AddDev</div>
+  )
+}
---
+++ src/components/course/rob/new copy/DevList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DevList = () => {
+  return (
+    <div>DevList</div>
+  )
+}
